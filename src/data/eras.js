6ec6c106b34c255a05f02c8942d5fd886f2f94a0
anjali <anjali.rawat8181@gmail.com>
2026-03-31 21:12:24 +0530

    Added era data

---
+++ src/data/eras.js
@@ -0,0 +1,43 @@
+const eras = [
+  {
+    id: "1950s",
+    name: "1950s",
+    color: "#f5e6cc",
+    artists: ["Elvis Presley", "Chuck Berry", "Little Richard"],
+    genres: ["Rock & Roll", "Blues", "Jazz"],
+    facts: [
+      "Birth of rock & roll",
+      "Rise of jukebox culture",
+      "Vinyl records dominate"
+    ],
+    song: "https://www.youtube.com/embed/ViZ3X1Y5k6w"
+  },
+  {
+    id: "1980s",
+    name: "1980s",
+    color: "#ff007f",
+    artists: ["Michael Jackson", "Madonna", "Prince"],
+    genres: ["Pop", "Synthwave", "Rock"],
+    facts: [
+      "MTV revolutionized music",
+      "Rise of music videos",
+      "Synthesizers everywhere"
+    ],
+    song: "https://www.youtube.com/embed/9bZkp7q19f0"
+  },
+  {
+    id: "2000s",
+    name: "2000s",
+    color: "#00aaff",
+    artists: ["Eminem", "Beyoncé", "Coldplay"],
+    genres: ["Hip Hop", "Pop", "R&B"],
+    facts: [
+      "iTunes changes music industry",
+      "Rise of digital music",
+      "CD era peak and decline"
+    ],
+    song: "https://www.youtube.com/embed/kffacxfA7G4"
+  }
+];
+
+export default eras;
