2c6d7c93688f480867aa315b30fbd426d0262c81
anjali <anjali.rawat8181@gmail.com>
2026-03-31 21:57:06 +0530

    Connected era data to app

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,16 @@
+import eras from "./data/eras";
+
 function App() {
   return (
     <div>
       <h1> Music Era Time Machine</h1>
+
+      {eras.map((era) => (
+        <div key={era.id}>
+          <h2>{era.name}</h2>
+          <p>{era.genres.join(", ")}</p>
+        </div>
+      ))}
     </div>
   );
 }
